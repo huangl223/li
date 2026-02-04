ff846de173e8614073211fcde50b74b64714fec3
Li Huang <huangl223@mail2.sysu.edu.cn>
2026-02-04 21:35:35 +0100

    update research page

--- assets/js/research.js
+++ assets/js/research.js
@@ -4,36 +4,31 @@ const researchTable = document.querySelector(".main");
 
 const research = [
   {
-    title: "A Batch Normalized Inference Network Keeps the KL Vanishing Away",
+    title: "Combining Tests and Proofs for Better Software Verification",
     authors:
-      "Qile Zhu, Wei Bi, Xiaojiang Liu, Xiyao Ma, Xiaolin Li and Dapeng Wu",
+      "Li Huang, Bertrand Meyer and Manuel Oriol",
     conferences:
-      "The 58th Annual Meeting of the Association for Computational Linguistics",
-    researchYr: 2020,
-    citebox: "popup1",
-    image: "assets/images/research-page/inteferenceNetwork.png",
-    citation: {
-      vancouver:
-        "Qile Zhu, Wei Bi, Xiaojiang Liu, Xiyao Ma, Xiaolin Li and Dapeng Wu. A Batch Normalized Inference Network Keeps the KL Vanishing Away. The 58th Annual Meeting of the Association for Computational Linguistics 2020.",
-    },
+      "Communications of the ACM",
+    researchYr: 2026,
+    image: "assets/images/research-page/combining_proofs_and_tests.png",
+    paperurl:
+      "https://www.arxiv.org/pdf/2601.16239",
     abstract:
-      "This is currently left empty and this can be considered as a dummy data 1",
+      "https://www.arxiv.org/pdf/2601.16239",
     absbox: "absPopup1",
   },
 
   {
-    title: "A Call for More Rigor in Unsupervised Cross-lingual Learning",
+    title: "Lessons from Formally Verified Deployed Software Systems",
     authors:
-      "Mikel Artetxe, Sebastian Ruder, Dani Yogatama, Gorka Labaka and Eneko Agirre",
+      "Li Huang, Sophie Ebersold, Alexander Kogtenkov, Bertrand Meyer, Yinling Liu",
     conferences:
-      "The 58th Annual Meeting of the Association for Computational Linguistics",
-    researchYr: 2020,
+      "ACM Computing Surveys (CSUR)",
+    researchYr: 2026,
     citebox: "popup2",
-    image: "assets/images/research-page/crossLingual.png",
-    citation: {
-      vancouver:
-        "Mikel Artetxe, Sebastian Ruder, Dani Yogatama, Gorka Labaka and Eneko Agirre. A Call for More Rigor in Unsupervised Cross-lingual Learning. The 58th Annual Meeting of the Association for Computational Linguistics 2020.",
-    },
+    image: "assets/images/research-page/deployed_verified_systems.png",
+    paperurl:
+      "https://dl.acm.org/doi/10.1145/3785652",
     abstract:
       "This is currently left empty and this can be considered as a dummy data 2",
     absbox: "absPopup2",
@@ -41,17 +36,14 @@ const research = [
 
   {
     title:
-      "A Comprehensive Analysis of Preprocessing for Word Representation Learning in Affective Tasks",
-    authors: "Nastaran Babanejad, Ameeta Agrawal, Aijun An and Manos Papagelis",
+      "Loop Unrolling: Formal Definition and Application to Testing",
+    authors: "Li Huang, Bertrand Meyer and Reto Weber",
     conferences:
-      "The 58th Annual Meeting of the Association for Computational Linguistics",
-    researchYr: 2020,
-    citebox: "popup3",
-    image: "assets/images/research-page/wordRepresentation.png",
-    citation: {
-      vancouver:
-        "Nastaran Babanejad, Ameeta Agrawal, Aijun An and Manos Papagelis. A Comprehensive Analysis of Preprocessing for Word Representation Learning in Affective Tasks. The 58th Annual Meeting of the Association for Computational Linguistics 2020.",
-    },
+      "IFIP International Conference on Testing Software and Systems",
+    researchYr: 2025,
+    image: "assets/images/research-page/loop_unrolling.png",
+    paperurl:
+      "https://link.springer.com/chapter/10.1007/978-3-032-05188-2_2",
     abstract:
       "This is currently left empty and this can be considered as a dummy data 3",
     absbox: "absPopup3",
@@ -59,88 +51,79 @@ const research = [
 
   {
     title:
-      "A Contextual Hierarchical Attention Network with Adaptive Objective for Dialogue State Tracking",
+      "Seeding Contradiction: a Fast Method for Generating Full-Coverage Test Suites",
     authors:
-      "Yong Shan, Zekang Li, Jinchao Zhang, Fandong Meng, Yang Feng, Cheng Niu and Jie Zhou",
+      "Li Huang, Bertrand Meyer, Manuel Oriol",
     conferences:
-      "The 58th Annual Meeting of the Association for Computational Linguistics",
-    researchYr: 2020,
+      "Springer Nature Computer Science",
+    researchYr: 2025,
     citebox: "popup4",
-    image: "assets/images/research-page/dialogueState.png",
-    citation: {
-      vancouver:
-        "Yong Shan, Zekang Li, Jinchao Zhang, Fandong Meng, Yang Feng, Cheng Niu and Jie Zhou. A Contextual Hierarchical Attention Network with Adaptive Objective for Dialogue State Tracking. The 58th Annual Meeting of the Association for Computational Linguistics 2020.",
-    },
+    image: "assets/images/research-page/seeding_contradiction.png",
+    paperurl:
+      "https://link.springer.com/article/10.1007/s42979-024-03482-0",
     abstract:
       "This is currently left empty and this can be considered as a dummy data 4",
     absbox: "absPopup4",
   },
 
   {
-    title: "Dual Super-Resolution Learning for Semantic Segmentation",
-    authors: "Wang, Li and Li, Dong and Zhu, Yousong and Tian, Lu and Shan, Yi",
+    title: "Is MCDC Really Better? Lessons from Combining Tests and Proofs",
+    authors:
+      "Li Huang, Bertrand Meyer, Manuel Oriol",
     conferences:
-      "IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR)",
-    researchYr: 2020,
+      "International Conference on Tests and Proofs",
+    researchYr: 2024,
     citebox: "popup5",
-    image: "assets/images/research-page/semanticSegmentation.png",
-    citation: {
-      vancouver:
-        "Wang, Li and Li, Dong and Zhu, Yousong and Tian, Lu and Shan, Yi. Dual Super-Resolution Learning for Semantic Segmentation. IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR) 2020.",
-    },
+    image: "assets/images/research-page/mcdc_branch.png",
+    paperurl:
+      "https://link.springer.com/chapter/10.1007/978-3-031-72044-4_2",
     abstract:
       "This is currently left empty and this can be considered as a dummy data 5",
     absbox: "absPopup5",
   },
 
+
   {
-    title: "Deep Unfolding Network for Image Super-Resolution",
-    authors: "Zhang, Kai and Van Gool, Luc and Timofte, Radu",
+    title: "Execution-Free Program Repair",
+    authors: "Li Huang, Bertrand Meyer, Ilgiz Mustafin, Manuel Oriol",
     conferences:
-      "IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR)",
-    researchYr: 2020,
+      "International Conference on the Foundations of Software Engineering - Idea, Vision and Relecftion track (FSE-IVR)",
+    researchYr: 2024,
     citebox: "popup6",
-    image: "assets/images/research-page/deepNetwork.png",
-    citation: {
-      vancouver:
-        "Zhang, Kai and Van Gool, Luc and Timofte, Radu. Deep Unfolding Network for Image Super-Resolution. IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR) 2020.",
-    },
-    abstract:
-      "This is currently left empty and this can be considered as a dummy data 6",
+    image: "assets/images/research-page/proof2fix.png",
+    paperurl:
+      "https://dl.acm.org/doi/10.1145/3663529.3663789",
     absbox: "absPopup6",
   },
 
   {
     title:
-      "Unsupervised Learning for Intrinsic Image Decomposition From a Single Image",
-    authors: "Liu, Yunfei and Li, Yu and You, Shaodi and Lu, Feng",
+      "A Failed Proof Can Yield a Useful Test",
+    authors: "Li Huang, Bertrand Meyer.",
     conferences:
-      "IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR)",
-    researchYr: 2020,
+      "Software Testing, Verification and Reliability (STVR)",
+    researchYr: 2023,
     citebox: "popup7",
-    image: "assets/images/research-page/imageDecomposition.png",
-    citation: {
-      vancouver:
-        "Liu, Yunfei and Li, Yu and You, Shaodi and Lu, Feng. Unsupervised Learning for Intrinsic Image Decomposition From a Single Image. IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR) 2020.",
-    },
+    image: "assets/images/research-page/proof2test.png",
+    paperurl:
+      "https://onlinelibrary.wiley.com/doi/full/10.1002/stvr.1859",
     abstract:
       "This is currently left empty and this can be considered as a dummy data 7",
     absbox: "absPopup7",
   },
+
   {
     title:
-      "Forward and Backward Information Retention for Accurate Binary Neural Networks",
+      "Improving Counterexample Quality from Failed Program Verification",
     authors:
-      "Qin, Haotong and Gong, Ruihao and Liu, Xianglong and Shen, Mingzhu and Wei, Ziran and Yu, Fengwei and Song, Jingkuan",
+      "Li Huang, Bertrand Meyer and Manuel Oriol",
     conferences:
-      "IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR)",
-    researchYr: 2020,
+      "IEEE International Symposium on Software Reliability Engineering Workshops (ISSREW)",
+    researchYr: 2022,
     citebox: "popup8",
-    image: "assets/images/research-page/neuralNetworks.jpg",
-    citation: {
-      vancouver:
-        "Qin, Haotong and Gong, Ruihao and Liu, Xianglong and Shen, Mingzhu and Wei, Ziran and Yu, Fengwei and Song, Jingkuan. Forward and Backward Information Retention for Accurate Binary Neural Networks. IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR) 2020.",
-    },
+    image: "assets/images/research-page/counterexample.png",
+    paperurl:
+      "https://ieeexplore.ieee.org/abstract/document/9985046/",
     abstract:
       "This is currently left empty and this can be considered as a dummy data 8",
     absbox: "absPopup8",
@@ -160,6 +143,7 @@ const fillData = () => {
       citation,
       absbox,
       abstract,
+      paperurl
     }) =>
       (output += `
             <tr data-aos="zoom-in-left"> 
@@ -170,31 +154,17 @@ const fillData = () => {
                             <img src="${image}" class="imgRes">
                         </span>
                     </div>
-                    <a href="#0" class="paperTitle"> ${title} </a> 
+                    <a href="${paperurl}" class="paperTitle"> ${title} </a> 
                     <div class = "authors"> ${authors} </div> 
                     
                     <div class="rConferences"> ${conferences} 
                         <div class="researchY">${researchYr}</div>
                     </div>
-                    
-                    <!--CITE BUTTON-->
-                    <div class="d-flex" style="margin-right:5%;">
-                        <button class="button button-accent button-small text-right button-abstract " type="button" data-toggle="collapse" data-target="#${absbox}" aria-expanded="false" aria-controls="${absbox}">
-                            ABSTRACT
-                        </button>
-                
-                        <button class="button button-accent button-small text-right button-abstract " type="button" data-toggle="collapse" data-target="#${citebox}" aria-expanded="false" aria-controls="${citebox}">
-                            CITE
-                        </button>
                     </div>
+                  
                     <div id="${absbox}" class="collapse" aria-labelledby="headingTwo" data-parent=".collapse">
                         <div class="card-body">
                             ${abstract}    
-                        </div>
-                    </div>
-                    <div id="${citebox}" class="collapse" aria-labelledby="headingTwo" data-parent=".collapse">
-                        <div class="card-body">
-                            ${citation.vancouver}    
                         </div>
                     </div>
                 </td>
