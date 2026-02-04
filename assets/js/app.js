1c22eff79ea44412c02a1a2a3378ca4866e4111d
Li Huang <huangl223@mail2.sysu.edu.cn>
2026-02-04 11:29:11 +0100

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,9 +27,6 @@ let header = $(`
    <li class="nav-item nav-item-hover"><a class="nav-link" href="research.html">Research</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="courses.html">Courses</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="https://fmai.hashnode.dev" target="_blank">Blogs</a></li>
-   <li class="nav-item">
-   <input type="checkbox" id="dark_toggler" class="dark_toggler" aria-label="Toggle Light Mode" onclick="toggle_light_mode()" checked>
-   </li>
    <div class="bike">
    <svg xmlns="http://www.w3.org/2000/svg" viewBox="-80 0 650 400" preserveAspectRatio="xMinYMin meet">
      <rect/>
